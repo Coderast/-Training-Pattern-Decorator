6c04ba01143f78922f8a81416c7e64ab8a4b8e2d
Coderast <Coderast@example.com>
2018-07-20 12:49:24 +0300

    Demo complited

--- DecoratorPattern/main.cpp
+++ DecoratorPattern/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "Modificators/SpeedBoostBlue/SpeedBoostBlue.h"
+#include "Modificators/AccelerationBoostRed/AccelerationBoostRed.h"
 int main()
 {
 	Car someCar(20., 5.);
 	SpeedBoostBlue someCarBoosted(&someCar);
+
 	std::cout << someCar.getSpeed() << std::endl;
-	std::cout << someCarBoosted.getSpeed() << std::endl;
+	std::cout << someCarBoosted.getSpeed() << std::endl << std::endl;
+
+	AccelerationBoostRed someCarVeryBoosted(&someCarBoosted);
+
+	std::cout << someCar.getAcceleration() << std::endl;
+	std::cout << someCarBoosted.getAcceleration() << std::endl;
+	std::cout << someCarVeryBoosted.getAcceleration() << std::endl;
+
 
 	system("pause");
 	return 0;
